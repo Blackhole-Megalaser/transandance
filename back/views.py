70c78d0695e53974aba5b94fc5640f70a7c7bee0
Khaïs COLIN <khais.colin@gmail.com>
2026-04-28 15:51:43 +0200

    feat: signup
    You can now signup for new accounts

--- back/views.py
+++ back/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -20,3 +23,8 @@ def profile(request):
         "back/profile.html",
         { "user": user },
     )
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
