bd4623e40d696cb0808b76b8fd8a83007c0bde62
cczerwin <cczerwin@student.42lehavre.fr>
2026-05-04 18:22:00 +0200

    Chat from the django tutorial accessible, require a redis server, TODO: add a dockerfile and change the structure to accomodate for it

--- back/views.py
+++ back/views.py
@@ -5,13 +5,9 @@ from django.contrib import messages
 
 from .forms import UserRegisterForm, UserModifyForm
 
-# Create your views here.
-
-
 def index(request):
     user = request.user
     return render(request, "back/index.html", {"user": user})
-
 
 @login_required
 def profile(request):
@@ -21,7 +17,6 @@ def profile(request):
         "back/profile.html",
         {"user": user},
     )
-
 
 def signup(request):
     if request.method == "POST":
