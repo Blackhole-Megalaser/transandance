c29b907549a73271e9dbd7f037ce1e7bfd8fc9d3
cczerwin <cczerwin@student.42lehavre.fr>
2026-05-05 17:17:59 +0200

    see above

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,6 +2,7 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -9,7 +10,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Join room group 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
