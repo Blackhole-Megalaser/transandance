e82d54f81a21cdffa859142a6c9a3477fb40adee
Sellith <leanevanbree@gmail.com>
2026-05-13 16:14:16 +0200

    update: added toggle to change theme from sm to lg screen size on navbar; completed navBar main options

---
+++ frontend/src/components/ThemeToggle.vue
@@ -0,0 +1,34 @@
+<template>
+  <button 
+    @click="theme.cycle()" 
+    class="themeBtn size-10 relative overflow-hidden flex-center"
+  >
+    <div class="m-auto size-8 absolute inset-y-0  flex-center rounded-full 
+      themeCursor active:size-6">
+      {{ theme.current === 'dark' ? "😈" : "🌸" }}
+    </div>
+  </button>
+</template>
+
+<script setup>
+import { useThemeStore } from '../storage/theme.js';
+
+const theme = useThemeStore();
+</script>
+
+<style scoped>
+@import "../style.css";
+
+.themeBtn {
+   @apply rounded-full bg-theme-button inset-shadow-sm 
+    inset-shadow-theme-button-shadow cursor-pointer
+}
+.themeCursor {
+  @apply rounded-full border bg-theme-cursor border-theme-cursor-border 
+    shadow-sm shadow-theme-cursor-shadow 
+}
+.flex-center {
+  @apply flex justify-center items-center
+}
+
+</style>
--- frontend/src/modules/home/HomeApp.vue
+++ frontend/src/modules/home/HomeApp.vue
@@ -1,12 +1,18 @@
 <template>
   <navBar
     @changeStatus="showSideBar = !showSideBar"
+    class="z-60"
   />
 
   <sideBar 
-    :class="showSideBar === false ? '-translate-x-60 rotate-360' : 'translate-x-0 -rotate-360'"
-    class="transition duration-200"
+    :class="!showSideBar ? '-translate-x-60 rotate-360' : 'translate-x-0 -rotate-360'"
+    class="transition duration-200 z-50"
   />
+  <div 
+    :class="showSideBar ? 'brightness-50' : 'brightness-100'"
+    class="transition duration-200 z-40"
+  >
+  </div>
 </template>
 
 <script setup>
@@ -14,7 +20,7 @@ import { useThemeStore } from '../../storage/theme.js';
 import { ref } from 'vue';
 
 import Button from '../../components/Button.vue';
-import NavBar from '../../components/NavBarHome.vue';
+import NavBar from '../../components/NavBar.vue';
 import SideBar from '../../components/SideBar.vue';
 
 const showSideBar = ref(false);
